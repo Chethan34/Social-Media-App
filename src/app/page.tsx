6d7e1c8f3720b3726a8fe9f5bade1b5dfee918b8
Chethan <Chethanprakash720@gmail.com>
2025-06-18 19:04:03 +0530

    feat: Sync with DB and Sidebar - fetching authenticated user data from clerkid - added responsive sidebar for both authenticated and unauthenticated case using shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 import {
   ClerkProvider,
   SignInButton,
@@ -10,24 +11,28 @@ import {
 } from "@clerk/nextjs";
 import ModeToggle from "@/components/ModeToggle";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <ClerkProvider>
-      <div className="m-4">
-        <SignedOut>
-          <SignInButton mode="modal">
-            <Button>
-              Sign In
-            </Button>
-          </SignInButton>
-        </SignedOut>
+    // <ClerkProvider>
+    //   <div className="m-4">
+    //     {/* <SignedOut>
+    //       <SignInButton mode="modal">
+    //         <Button>
+    //           Sign In
+    //         </Button>
+    //       </SignInButton>
+    //     </SignedOut>
 
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <ModeToggle/>
-        <Button variant={"secondary"}>Click me</Button>
-      </div>
-    </ClerkProvider>
+    //     <SignedIn>
+    //       <UserButton />
+    //     </SignedIn>
+    //     <ModeToggle/>
+    //     <Button variant={"secondary"}>Click me</Button> */}
+    //   </div>
+    // </ClerkProvider>
+
+    <div className="m-4">
+      home page
+    </div>
   );
 }
